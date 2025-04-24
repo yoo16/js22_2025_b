8965015ee8994ba1e3e3ea8ef4c14ee8a679b3aa
yoo16 <yohei.yoshikawa@gmail.com>
2025-04-24 09:29:48 +0900

    first commit

---
+++ 20_jquery_2/js/selector.js
@@ -0,0 +1,59 @@
+$(() => {
+    // TODO: id=title
+    var titleElement
+    console.log(titleElement)
+
+    // TODO: class=city
+    var cityElements
+    console.log(cityElements)
+
+    // TODO: id=jp-city の class=city
+    var jpCityList
+    console.log(jpCityList)
+
+    // TODO: id=us-city
+    var usCityList
+    console.log(usCityList)
+
+    // TODO: html() 取得
+    var title
+    console.log(title)
+
+    // TODO: html() 設定
+
+    // TODO: .city に addClass()
+    // bg-gray-300
+
+    // TODO: .city に removeClass()
+    // bg-gray-300
+
+    // TODO: get(): .city に指定インデックスで取得(バニラJS)
+
+    // TODO: eq(): .city に指定インデックスで取得(jQueryオブジェクト)q
+
+    // TODO: eq(): .city の最初を取得し、class=bg-red-500 を追加
+
+    // TODO: eq(): .city の最初を取得し、class=bg-red-500 を追加
+
+    // TODO: first(): .city の最初を取得し、class=bg-red-500 を追加
+
+    // TODO: .city の最後を取得し、class=bg-red-500 を追加
+
+    // TODO: .city をすべて取得し、class=bg-red-300 を削除
+
+    // TODO: filter(): 指定した条件に一致した要素を選択
+    // .city をすべて取得し、奇数に class=bg-gray-200 を追加
+
+    // TODO: not(): 指定した条件に一致しない要素を選択
+    // 奇数以外（偶数）の要素に class=bg-blue-300 を追加 
+
+    // TODO: find(): 子孫要素を取得
+
+    // TODO: slice(): 選択した要素を部分的に取得
+    // 2番目から3番目の要素を取得し、class=bg-orange-500 を追加
+
+    // nth-child()
+    // TODO: id=us-city の class=city のインデックス「2」を選択
+    var selectedCity
+    // TODO: text() でコンテンツ表示
+})
