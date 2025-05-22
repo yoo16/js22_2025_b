e5fc38a05f80a127208f9d1d302508e1f8d25819
yoo16 <yohei.yoshikawa@gmail.com>
2025-05-22 12:22:23 +0900

    update

--- 27_jquery_parallax/js/parallax1.js
+++ 27_jquery_parallax/js/parallax1.js
@@ -4,26 +4,30 @@ $(document).ready(function () {
 
     // TODO: スクロール: jqScroll
     $(window).scroll(function () { 
-        const scrollY = $(this).scrollTop();
-        console.log(scrollY);
-    });
-    // $(window).on('scroll', function () {
-        
-    // });
-
         // TODO: scrollY: スクロール位置を取得し、0.5 をかける
-        // jqScrollTopGet
-        const scrollY = 0
+        const scrollY = $(this).scrollTop() * 0.5;
+        // console.log(scrollY);
+
         // TODO: parallaxHeight: パララックス対象の高さ取得
-        // jqOuterHeight
-        const parallaxHeight = 0
+        const parallaxHeight = $parallax.outerHeight();
 
         // TODO: バックグラウンド画像を上へ移動：background-position-y: -scrollY px
-        // jqCssSet
+        $parallax.css({
+            'background-position-y': -scrollY + 'px',
+            'background-position-x': -scrollY + 'px',
+        });
 
         // 文字コンテンツの移動距離計算
         const translateY = Math.min(parallaxHeight / 2, scrollY * 0.2);
 
         // TODO: 文字の位置を設定： transform: translateY(translateY px)
-        // jqCssSet
+        $parallaxContent.css({
+            'transform': 'translateY(' + translateY + 'px)',
+        });
+
+    });
+    // $(window).on('scroll', function () {
+        
+    // });
+
 });
